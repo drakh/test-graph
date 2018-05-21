e20367ab4ff9caf9856aa1aaca3ee02cf2a72d1a
Lubomir Panak <lubopanak@gmail.com>
2018-05-21 11:48:15 +0200

    - simplify the form in which frontend will use the data

--- src/server/api/routes.ts
+++ src/server/api/routes.ts
@@ -1,16 +1,49 @@
-import { wrap } from 'async-middleware';
-import { Router, Request, Response, NextFunction } from 'express';
-import * as path from 'path';
 import * as fs from 'fs-extra';
+import { wrap } from 'async-middleware';
+import { NextFunction, Request, Response, Router } from 'express';
+import { baseDir } from 'common/constants';
+import { nodeId, NodeItem, NodeRelation, RawData } from 'common/types';
 
-export async function serveData(_req: Request, res: Response, _next: NextFunction) {
-    const fileName = path.join(__dirname, '../../../data/10000.json');
-    const data = await fs.readJson(fileName);
+async function loadRawData(): Promise<RawData> {
+    const fileName = `${baseDir}/10000.json`;
+    const data: RawData = await fs.readJson(fileName);
+    return data;
+}
+
+async function simlifyData(): Promise<NodeItem[]> {
+    const originalData = await loadRawData();
+    const relationsMap = new Map<nodeId, NodeRelation[]>();
+    originalData.edges.map(edge => {
+        const relation = relationsMap.get(edge.data.source) || [];
+        relation.push({
+            targetId: edge.data.target,
+            label: edge.data.label,
+            weight: 1, // just dummy weight
+        });
+        relationsMap.set(edge.data.source, relation);
+    });
+    const nodes: NodeItem[] = originalData.nodes.map(node => {
+        return {
+            id: node.data.id,
+            data: {
+                wallets: node.data.wallets,
+                label: node.data.label,
+            },
+            position: node.position,
+            targets: relationsMap.get(node.data.id),
+        };
+    });
+    return nodes;
+}
+
+export async function serveRawData(_req: Request, res: Response, _next: NextFunction) {
+    // this form of data should be returned by server
+    const data = await simlifyData();
     res.json(data);
 }
 
 export function register(app: Router): void {
     const router = Router();
-    router.get('/', wrap(serveData));
+    router.get('/', wrap(serveRawData));
     app.use('/nodes', router);
 }
