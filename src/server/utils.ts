03bdb022cba9de4554f0ec82b264a903effb9a76
Lubomir Panak <lubopanak@gmail.com>
2018-05-21 12:51:39 +0200

    - register webpack hot reload middleware

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,6 +1,10 @@
 import * as express from 'express';
 import { apiPrefix } from 'common/constants';
+import * as webpack from 'webpack';
+import * as webpackDevMiddleware from 'webpack-dev-middleware';
+import * as webpackHotMiddleware from 'webpack-hot-middleware';
 import * as path from 'path';
+import config from 'webpack/config';
 
 export function registerApi(app: express.Application, register: Function): void {
     const router = express.Router();
@@ -14,8 +18,22 @@ export function registerApp(app: express.Application, register: Function): void 
     app.use('/', appRouter);
 }
 
+function registerWebpack(router: express.Application) {
+    const compiler = webpack(config);
+    const middleware = webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+        serverSideRender: true,
+    });
+    router.use(middleware);
+    router.use(webpackHotMiddleware(compiler, {
+        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
+    }));
+
+}
+
 export function setupApp(): express.Application {
     const app = express();
+    registerWebpack(app);
     app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
         res.locals.req = req;
         next();
