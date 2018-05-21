7e0d1d92cac496250a56ac5244b8c5e87c4c816c
Lubomir Panak <lubopanak@gmail.com>
2018-05-21 12:04:32 +0200

    - otuput directory

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,3 +3,4 @@ import * as path from 'path';
 export const apiPrefix = '/api';
 export const appPort = 8080;
 export const baseDir = path.join(__dirname, '../../data');
+export const outPath = path.join(__dirname, '../../dist/');
