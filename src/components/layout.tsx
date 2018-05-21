04d9561fe4c1c3598c04fa75b96fda7c8fddd022
Lubomir Panak <lubopanak@gmail.com>
2018-05-21 10:40:26 +0200

    - initialise layout with mount point

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
+import { mountPoints } from 'common/mount-points';
 
 export interface Props {
 }
@@ -13,8 +14,11 @@ export class Layout extends React.Component<Props, State> {
             <html lang="en">
             <head>
                 <title>Graphs</title>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
             </head>
             <body>
+            <main id={mountPoints.APP}/>
             </body>
             </html>
         );
