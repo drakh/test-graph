f001476093ce4b4d456b74475fff412cf35abb9e
Lubomir Panak <lubopanak@gmail.com>
2018-05-21 10:46:36 +0200

    - serve data

--- src/server/api/routes.ts
+++ src/server/api/routes.ts
@@ -1,6 +1,16 @@
-import { Router } from 'express';
+import { wrap } from 'async-middleware';
+import { Router, Request, Response, NextFunction } from 'express';
+import * as path from 'path';
+import * as fs from 'fs-extra';
+
+export async function serveData(_req: Request, res: Response, _next: NextFunction) {
+    const fileName = path.join(__dirname, '../../../data/10000.json');
+    const data = await fs.readJson(fileName);
+    res.json(data);
+}
 
 export function register(app: Router): void {
     const router = Router();
-    app.use('/', router);
+    router.get('/', wrap(serveData));
+    app.use('/nodes', router);
 }
