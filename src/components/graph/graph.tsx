f16d032f2ae0e92465a7c53949c8ab8663a291f2
Lubomir Panak <lubopanak@gmail.com>
2018-05-21 12:08:15 +0200

    - basic data loading

--- src/components/graph/graph.tsx
+++ src/components/graph/graph.tsx
@@ -1,13 +1,39 @@
 import * as React from 'react';
+import { NodeItem } from '../../common/types';
+import { api } from './client/api';
 
 export interface Props {
 }
 
 export interface State {
+    data: NodeItem[];
 }
 
 export class Graph extends React.Component<Props, State> {
+    public constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        };
+    }
+
+    public async componentDidMount() {
+        await this.load();
+    }
+
     public render() {
         return (<h1>Hello world</h1>);
+    }
+
+    private async load() {
+        try {
+            const res = await api.data.load();
+            this.setState({
+                data: res.data,
+            });
+        }
+        catch (e) {
+            console.error('Error loading data');
+        }
     }
 }
